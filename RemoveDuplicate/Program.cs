6ada6e980ea565849b31524127fe4ff76da6a705
VivekDeshmukh98 <vvkd98@gmail.com>
2023-08-24 17:56:13 +0530

    updated

--- RemoveDuplicate/Program.cs
+++ RemoveDuplicate/Program.cs
@@ -4,9 +4,35 @@ namespace RemoveDuplicate
 {
     class Program
     {
+        static int RemoveDuplicates(int[] array)
+        {
+            if (array.Length == 0)
+                return 0;
+
+            int newSize = 1;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                if(array[i] != array[newSize - 1])
+                {
+                    array[newSize] = array[i];
+                    newSize++;
+                }
+            }
+            return newSize;
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] sortedArray = { 10, 20, 30, 30, 40, 50, 60, 70 };
+            int newLength = RemoveDuplicates(sortedArray);
+
+            Console.WriteLine("Modified Array: ");
+
+            for (int i = 0; i < newLength; i++)
+            {
+                Console.Write(sortedArray[i] + " ");
+            }
         }
     }
 }
---
+++ RotateArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RotateArray
+{
+    class Program
+    {
+        static void RotateArray(int[] array, int positions)
+        {
+            int length = array.Length;
+            int[] rotatedArray = new int[length];
+
+            for(int i=0; i < length; i++)
+            {
+                int newPosition = (i + positions) % length;//2
+                rotatedArray[newPosition] = array[i];
+
+            }
+
+            for(int i = 0; i < length; i++)
+            {
+                array[i] = rotatedArray[i];
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+            int positions = 2;
+
+            RotateArray(numbers, positions);
+
+            foreach(int number in numbers)
+            {
+                Console.Write(number+ " ");
+            }
+        }
+    }
+}
