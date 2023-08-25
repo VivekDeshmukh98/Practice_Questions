d62977f8f6cb0d154bfc1d157403821cef15940b
VivekDeshmukh98 <vvkd98@gmail.com>
2023-08-25 18:02:30 +0530

    updated

---
+++ MaxSubArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MaxSubArray
+{
+    class Program
+    {
+        public static int MaxSubArray(int[] nums)
+        {
+            int maxSum = nums[0];
+            int currentSum = nums[0];
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                currentSum = Math.Max(nums[i], currentSum + nums[i]);
+                maxSum = Math.Max(maxSum, currentSum);
+            }
+            return maxSum;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            int result = MaxSubArray(nums);
+            Console.WriteLine("Maximum Subarray Sum: " + result);
+        }
+    }
+}
