28d8309e81b7dd55231d70e789bf3c90976f5c58
VivekDeshmukh98 <vvkd98@gmail.com>
2023-08-25 17:26:18 +0530

    updated new

---
+++ ReverseString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        public static void ReverseString(char[] s)
+        {
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                char temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+        static void Main(string[] args)
+        {
+            char[] s = { 'q', 's', 'h', 'c', 'a', 'h' };
+            Console.WriteLine("Original String : " + new string(s));
+
+            ReverseString(s);
+            Console.WriteLine("Original String : " + new string(s));
+
+
+        }
+    }
+}
