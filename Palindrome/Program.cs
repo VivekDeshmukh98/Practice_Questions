265053392a7e7caa05e9a249d2647fcc674f488d
VivekDeshmukh98 <vvkd98@gmail.com>
2023-08-25 18:23:52 +0530

    palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Palindrome
+{
+    class Program
+    {
+        public static bool IsPalindrome(string s)
+        {
+            //Convert the string to lowercase and remove non-alphanumeric characters
+            string cleanedstring = new string(s.ToLower().ToCharArray().Where(C => Char.IsLetterOrDigit(c)).ToArray());
+
+            int left = 0;
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
