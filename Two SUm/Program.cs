372f144783ec54e944edb6c3e646755029c1cbc8
VivekDeshmukh98 <vvkd98@gmail.com>
2023-08-30 17:53:46 +0530

    30-08

---
+++ Two SUm/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Two_SUm
+{
+    public static int[] SumTwo(int[] nums, int target)
+    {
+        Dictionary<int, int> numToIndex = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (numToIndex.ContainsKey(complement))
+            {
+                return new int[] { numToIndex[complement], i };
+            }
+
+            numToIndex[nums[i]] = i;
+        }
+
+        throw new ArgumentException("No two numbers add up to the target sum.");
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 18;
+
+            int[] result = TwoSumm(nums, target);
+            Console.WriteLine($"Indices of two numbers: {result[0]}, {result[1]}");
+        }
+    }
+}
---
+++ TwoSum_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoSum_1
+{
+    class Program
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> numToIndex = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (numToIndex.ContainsKey(complement))
+                {
+                    return new int[] { numToIndex[complement], i };
+                }
+
+                numToIndex[nums[i]] = i;
+            }
+
+            throw new ArgumentException("No two numbers add up to the target sum.");
+        }
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 18;
+
+            int[] result = TwoSum(nums, target);
+            Console.WriteLine($"Indices of two numbers: {result[0]}, {result[1]}");
+        }
+    }
+}
