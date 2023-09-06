d0da3d46fcf59fe15bc813b4ca4cc550a308eac2
VivekDeshmukh98 <vvkd98@gmail.com>
2023-09-06 14:27:41 +0530

    new

---
+++ RotateArray_2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace RotateArray_2
+{
+
+    static void ReverseArray(int[] array)
+    {
+        int start = 0;
+        int end = array.Length - 1;
+
+        while (start < end)
+        {
+            //swap elements at start and end
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+
+            //Move towards the center
+            start++;
+            end--;
+
+        }
+
+    }
+    static void Main(string[] args)
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        Console.WriteLine("Original array");
+
+        //foreach (int element in numbers)
+        //{
+        //    Console.WriteLine($"{element}");
+        //}
+
+        string arrayAsString = string.Join(", ", numbers);
+
+        Console.WriteLine(arrayAsString);
+
+        ReverseArray(numbers);
+
+        Console.WriteLine("Reveresed Array");
+        string ArrayAsString = string.Join(", ", numbers);
+        Console.WriteLine(ArrayAsString);
+
+
+
+    }
+}
+}
---
+++ ValidParenthesis/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValidParenthesis
+{
+    class Program
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')' && (stack.Count == 0 || stack.Pop() != '('))
+                {
+                    return false;
+                }
+                else if (c == ']' && (stack.Count == 0 || stack.Pop() != '['))
+                {
+                    return false;
+                }
+                else if (c == '}' && (stack.Count == 0 || stack.Pop() != '{'))
+                {
+                    return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValid("()"));
+            Console.WriteLine(IsValid("({})"));
+            Console.WriteLine(IsValid("{)(("));
+        }
+    }
+}
