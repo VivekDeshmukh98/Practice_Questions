4be5831cc8e1ee97105fb411ff8e6be47110f623
YOGINI <YOGINI@DESKTOP-18RQ993>
2023-09-13 21:34:55 +0530

    update

---
+++ FactorialCalculation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialCalculation
+{
+    class Program
+    {
+        static long CalculateFactorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Input must be non-negative");
+            }
+
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            return n * CalculateFactorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+
+            int n = 5;
+            long factorial = CalculateFactorial(n);
+            Console.WriteLine($"Factorial of {n} is: {factorial}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void GenerateFibonacci(int n)
+        {
+            long[] fibonacci = new long[n];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for(int i = 2; i <n; i++)
+            {
+                fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+            }
+
+            Console.WriteLine("Fibonacci Sequence (iterative):");
+
+            foreach (long item in fibonacci)
+            {
+                Console.WriteLine(item+ " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = 10;
+            GenerateFibonacci(n);
+            Console.ReadLine();
+        }
+    }
+}
